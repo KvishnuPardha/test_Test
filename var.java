0f21ad052412fe36f03eeb77c3df9364360e5aaf
Kvishnupardha <vkandimalla2s@semo.edu>
2025-01-01 09:13:12 -0600

    additional data is created

--- var.java
+++ var.java
@@ -8,7 +8,7 @@ public class var {
         double myDouble = 5.99;
         System.out.println("Double: " + myDouble);
 
-        // Character variable
+        // Character variable (single quotes)
         char myChar = 'A';
         System.out.println("Character: " + myChar);
 
@@ -19,5 +19,25 @@ public class var {
         // Boolean variable
         boolean myBool = true;
         System.out.println("Boolean: " + myBool);
+        // Method overloading demonstration
+        System.out.println("Sum of 2 and 3: " + sum(2, 3));
+        System.out.println("Sum of 2.5 and 3.5: " + sum(2.5, 3.5));
+        System.out.println("Sum of 1, 2, and 3: " + sum(1, 2, 3));
+        }
+
+        // Method to sum two integers
+        public static int sum(int a, int b) {
+        return a + b;
+        }
+
+        // Method to sum two doubles
+        public static double sum(double a, double b) {
+        return a + b;
+        }
+
+        // Method to sum three integers
+        public static int sum(int a, int b, int c) {
+        return a + b + c;
+        }
     }
 }
