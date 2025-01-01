ddca7c51312644e92e943a0c5941a50612ba2fcb
Kvishnupardha <vkandimalla2s@semo.edu>
2025-01-01 09:11:25 -0600

    source contrl

---
+++ var.java
@@ -0,0 +1,23 @@
+public class var {
+    public static void main(String[] args) {
+        // Integer variable
+        int myInt = 10;
+        System.out.println("Integer: " + myInt);
+
+        // Double variable
+        double myDouble = 5.99;
+        System.out.println("Double: " + myDouble);
+
+        // Character variable
+        char myChar = 'A';
+        System.out.println("Character: " + myChar);
+
+        // String variable
+        String myString = "Hello, World!";
+        System.out.println("String: " + myString);
+
+        // Boolean variable
+        boolean myBool = true;
+        System.out.println("Boolean: " + myBool);
+    }
+}
